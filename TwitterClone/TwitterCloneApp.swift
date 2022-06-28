442c19a58b859b005e007e1070e2a80590b14d9c
Dheeraj <dheeraj@ascentspark.com>
2022-06-29 00:02:44 +0530

    Firebase added

--- TwitterClone/TwitterCloneApp.swift
+++ TwitterClone/TwitterCloneApp.swift
@@ -7,9 +7,13 @@
 
 //27th June 2022 : Dheeraj : 2:24:13 : UI till login signup done.
 import SwiftUI
+import Firebase
 
 @main
 struct TwitterCloneApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             NavigationView{
